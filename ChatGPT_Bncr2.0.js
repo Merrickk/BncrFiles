9b581f7794f5ae466a85031c3247b217be2d30e5
merrick <69239399+Merrickk@users.noreply.github.com>
2024-04-12 22:23:48 +0800

    跟进修改调用模块

--- ChatGPT_Bncr2.0.js
+++ ChatGPT_Bncr2.0.js
@@ -2,11 +2,11 @@
  * @author Merrick
  * @name ChatGPT_BNCR2.0
  * @origin Merrick
- * @version 1.0.1
+ * @version 1.0.2
  * @description ChatGpt聊天，适配无界2.0
  * @rule ^(ai) ([\s\S]+)$
  * @rule ^(ai)$
- * @admin false
+ * @admin true
  * @public false
  * @priority 99999
  * @disable false
@@ -17,8 +17,9 @@
 需要ApiKey，没有的可以看看这个项目https://github.com/aurora-develop/aurora
 
 v1.0.1 修复了QQ和微信平台在编辑模式下不能正常回复的bug
+v1.0.2 跟进大佬修改了调用chatgpt模块，用got发送请求（虽然我不懂但是我会复制粘贴）
 
-todo
+todo（其实是我想做不会做，大佬带我~~~~~）
 1.用更优雅的方式实现HumanTG的编辑回复和直接回复的切换
 2.选择预设角色的时候可以把Prompt直接显示出来，方便调整（下拉列表框和文本框联动）
 3.添加画图功能（大佬的代码已经实现了，我没有4.0的ApiKey，无法测试）
@@ -26,6 +27,7 @@ todo
 
 const fs = require('fs');
 const path = require('path');
+const got = require('got');
 const promptFilePath = './mod/prompts.json';
 const fullPath = path.join(__dirname, promptFilePath);
 // 读取prompts
@@ -70,10 +72,7 @@ module.exports = async s => {
     const apiBaseUrl = CDB.apiBaseUrl;
     const isEdit = CDB.isEdit;
     const promptDiy = CDB.promptDiy;
-    const { ChatGPTAPI } = await import('chatgpt');
-    let api = {};
-    api = initializeChatGPTAPI(apiKey, apiBaseUrl, modeNames[CDB.modeSel]);
-    let opt = { timeoutMs: 60 * 1000 };
+    let gptAPI = initializeChatGPTAPI(apiKey, apiBaseUrl, modeNames[CDB.modeSel]);
     if (s.param(1) === 'ai') {
         let prompt = '';
         if (!promptDiy) {
@@ -82,63 +81,62 @@ module.exports = async s => {
             prompt = promptDiy;
         }
         let history = [{
-            role: 'system', content: [{ type: "text", text: prompt + '，另外，输出字符限制，输出50-100字' }]
+            role: 'system', content:  prompt + '，另外，输出字符限制，输出50-100字'
         }]
         await relpyMod(s, isEdit, `Let me see...`);
-        history.push({ role: 'user', content: [{ type: "text", text: s.param(2) }] });
+        history.push({ role: 'user', content: s.param(2) });
         let response
         try {
-            response = await api.sendMessage(JSON.stringify(history), opt);
-            // console.log(response);
-            await handleResponse(response, history)
-            await continuousDialogue(api, opt);
+            let response = await fetchOpenAICompletions(gptAPI, history);
+            history.push({
+                role: 'assistant', content: response
+            });
+            await relpyMod(s, isEdit, response);
+            await continuousDialogue(gptAPI);
         }
         catch (error) {
             handleError(error);
             //如果错误信息包含OpenAI error 429，使用gpt3.5模型继续调用
             if (error.toString().indexOf('OpenAI error 429') !== -1) {
-                relpyMod("gpt4模型调用失败，正在使用gpt3.5模型");
-                api = initializeChatGPTAPI(apiKey, apiBaseUrl, 'gpt-3.5-turbo');
+                await relpyMod(s, isEdit, "gpt4模型调用失败，正在使用gpt3.5模型");
                 try {
-                    response = await api.sendMessage(JSON.stringify(history), opt);
-                    history.push({ role: 'assistant', content: [{ type: "text", text: response.text }] });
-                    await relpyMod(s, isEdit, response.text);
-                    await continuousDialogue(api, opt);
+                    gptAPI = initializeChatGPTAPI(apiKey, apiBaseUrl, 'gpt-3.5-turbo');
+                    let response = await fetchOpenAICompletions(gptAPI, history);
+                    history.push({ role: 'assistant', content: response });
+                    await relpyMod(s, isEdit, response);
+                    await continuousDialogue(gptAPI);
                 }
                 catch (error) {
                     handleError(error);
                     return;
                 }
-
             }
             return;
         }
 
-        async function continuousDialogue(api, opt) {
+        async function continuousDialogue(gptAPI) {
             while (true) {
                 let input = await s.waitInput(() => { }, 60);
                 if (!input) {
-                    await relpyMod(s, isEdit, '对话超时。');
+                    await relpyMod(s, isEdit, "对话超时。");
                     break;
                 }
-
                 input = input.getMsg();
                 if (input.toLowerCase() === 'q') {
-                    await relpyMod(s, isEdit, '对话结束。');
+                    await relpyMod(s, isEdit, "对话结束。");
                     break;
                 }
-
                 history.push({
-                    role: 'user', content: [{ type: "text", text: input }]
+                    role: 'user', content: input
                 });
-
                 let response;
                 try {
-                    response = await api.sendMessage(JSON.stringify(history), opt);
+                    response = await fetchOpenAICompletions(gptAPI, history);
                     if (response) {
-                        await handleResponse(response, history)
+                        history.push({ role: 'assistant', content: response });
+                        await relpyMod(s, isEdit, response);
                     } else {
-                        await relpyMod(s, isEdit, '没有收到回答。');
+                        throw new Error('continuousDialogue error');
                     }
                 } catch (error) {
                     handleError(error);
@@ -163,7 +161,6 @@ module.exports = async s => {
                     type: 'text',
                 });
             } else {
-                console.log(isEdit, userId, platform, text);
                 sysMethod.push({
                     platform: platform,
                     userId: userId,
@@ -175,79 +172,47 @@ module.exports = async s => {
         
     }
 
-    async function handleResponse(response, history) {
-        // console.log(response);
-        if (isValidFormat(response.text)) {
-            const result = processText(response.text);
-            const link = result.link;
-            const lastLine = result.lastLine;
-            sendImg(platform, link);
-            history.push({
-                role: 'assistant', content: [{ type: "image_url", image_url: { url: link } }, { type: "text", text: lastLine }]
-            });
-            await relpyMod(s, isEdit, lastLine);
-            return history;
-        }
-        else {
-            let text = removeUrls(response.text);
-            await relpyMod(s, isEdit, text);
-            history.push({
-                role: 'assistant', content: [{ type: "text", text: text }]
-            });
-            return history;
-        }
-    }
-
-    
-
-    function removeUrls(text) {
-        // 正则表达式匹配大多数网址格式
-        const urlRegex = /https?:\/\/[^\s]+|www\.[^\s]+/gi;
-        // 将所有匹配到的网址替换为空字符串
-        return text.replace(urlRegex, '');
-    }
-
-    function isValidFormat(text) {
-        // 正则表达式来检测文本格式
-        // 检测图像链接格式
-        const imageRegex = /!\[image\]\(https?:\/\/.*?\)/;
-        // 检测下载链接格式
-        const downloadLinkRegex = /\[下载链接\]\(https?:\/\/.*?\)/;
-
-        return imageRegex.test(text) && downloadLinkRegex.test(text);
-    }
-
-    function processText(text) {
-        // 将文本按行分割
-        const lines = text.split('\n');
-        // 初始化变量来存储链接和最后一行文本
-        let link = '';
-        let lastLine = '';
-        // 遍历每一行来查找链接
-        lines.forEach(line => {
-            if (line.includes('[下载链接]')) {
-                // 提取链接
-                const linkMatch = line.match(/\((.*?)\)/);
-                if (linkMatch && linkMatch.length > 1) {
-                    link = linkMatch[1];
-                }
-            }
-        });
-        // 获取最后一行文本
-        if (lines.length > 0) {
-            lastLine = lines[lines.length - 1];
-        }
-        return { link, lastLine };
-    }
-
     function initializeChatGPTAPI(apiKey, baseUrl, model) {
-        return new ChatGPTAPI({
+        return {
             apiKey: apiKey,
             apiBaseUrl: baseUrl,
             completionParams: {
                 model: model
             }
+        };
+    }
+
+    async function fetchOpenAICompletions(gptAPI, messages, timeout = 30000) {
+        const fetchPromise = got.post(`${gptAPI.apiBaseUrl}/chat/completions`, {
+            json: {
+                model: gptAPI.completionParams.model,
+                messages: messages,
+                stream: false
+            },
+            responseType: 'json',
+            timeout: timeout,
+            headers: {
+                'Authorization': `Bearer ${gptAPI.apiKey}`
+            }
         });
+
+        try {
+            const response = await fetchPromise;
+
+            if (response.statusCode !== 200) {
+                throw new Error(`HTTP error! status: ${response.statusCode}`);
+            }
+            const res = response.body.choices[0].message.content;
+            return res;
+        } catch (error) {
+            console.error(`Fetch error: ${error.message}`);
+            console.error(`Error stack: ${error.stack}`);
+            if (error.response) {
+                console.error(`Response status: ${error.response.statusCode}`);
+                console.error(`Response data: ${JSON.stringify(error.response.body, null, 2)}`);
+            }
+            return null;
+        }
     }
 
     function handleError(error) {
@@ -273,3 +238,4 @@ module.exports = async s => {
         }
     }
 };
+
